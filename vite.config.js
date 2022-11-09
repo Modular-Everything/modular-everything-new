0deda493e7aeae39038823d8943ae6d14520225b
Chrish Dunne <yo@modular-everything.com>
2022-11-09 15:30:15 +0000

    feat: Testing out multiple entry points

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
+import { resolve } from "path";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
+const root = resolve(__dirname, "src/pages");
+const outDir = resolve(__dirname, "dist");
+const pages = resolve(root, "pages");
+const input = {
+  main: resolve(root, "index.html"),
+  about: resolve(root, "about", "index.html"),
+};
+
 export default () => {
   return defineConfig({
+    root,
+    build: {
+      outDir,
+      emptyOutDir: true,
+      rollupOptions: {
+        input,
+      },
+    },
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src/app", import.meta.url)),
