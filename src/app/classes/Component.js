d385d377ebca8c73e5cd2e8bfde7c490936c55d9
Chrish Dunne <yo@modular-everything.com>
2022-11-13 08:11:27 +0000

    feat: Extend Page to Component class

---
+++ src/app/classes/Component.js
@@ -0,0 +1,58 @@
+export const Component = class Component {
+  constructor({ id, element, elements }) {
+    this.id = id;
+    this.selector = element;
+    this.selectors = { ...elements };
+  }
+
+  create() {
+    this.initElement(this.selector);
+    this.initElements(this.selectors);
+
+    console.log(`Created component: ${this.id}`);
+  }
+
+  show(anim) {
+    return new Promise(async (resolve) => {
+      if (anim) {
+        await anim.play();
+      } else {
+        console.warn(`Component ${this.id} doesn't have an animation set.`);
+      }
+
+      resolve();
+    });
+  }
+
+  initElement(selector) {
+    if (!selector) {
+      return;
+    }
+
+    if (selector instanceof window.HTMLElement) {
+      this.element = selector;
+    } else {
+      this.element = document.querySelector(selector);
+    }
+  }
+
+  initElements(selectors) {
+    this.elements = {};
+
+    for (const key in selectors) {
+      const selector = selectors[key];
+      if (selector === window) {
+        this.elements[key] = window;
+      } else if (selector instanceof window.HTMLElement) {
+        this.elements[key] = selector;
+      } else {
+        this.elements[key] = document.querySelectorAll(selector);
+        if (this.elements[key].length === 0) {
+          this.elements[key] = null;
+        } else if (this.elements[key].length === 1) {
+          this.elements[key] = this.elements[key][0];
+        }
+      }
+    }
+  }
+};
--- src/app/classes/Page.js
+++ src/app/classes/Page.js
@@ -1,58 +1,11 @@
-export const Page = class Page {
+import { Component } from "./Component";
+
+export const Page = class Page extends Component {
   constructor({ id, element, elements }) {
-    this.id = id;
-    this.selector = element;
-    this.selectors = { ...elements };
+    super({ id, element, elements });
   }
 
   create() {
-    this.initElement(this.selector);
-    this.initElements(this.selectors);
-
-    console.log(`Created page: ${this.id}`);
-  }
-
-  show(anim) {
-    return new Promise(async (resolve) => {
-      if (anim) {
-        await anim.play();
-      } else {
-        console.warn(`Page ${this.id} doesn't have an animation set.`);
-      }
-
-      resolve();
-    });
-  }
-
-  initElement(selector) {
-    if (!selector) {
-      return;
-    }
-
-    if (selector instanceof window.HTMLElement) {
-      this.element = selector;
-    } else {
-      this.element = document.querySelector(selector);
-    }
-  }
-
-  initElements(selectors) {
-    this.elements = {};
-
-    for (const key in selectors) {
-      const selector = selectors[key];
-      if (selector === window) {
-        this.elements[key] = window;
-      } else if (selector instanceof window.HTMLElement) {
-        this.elements[key] = selector;
-      } else {
-        this.elements[key] = document.querySelectorAll(selector);
-        if (this.elements[key].length === 0) {
-          this.elements[key] = null;
-        } else if (this.elements[key].length === 1) {
-          this.elements[key] = this.elements[key][0];
-        }
-      }
-    }
+    super.create();
   }
 };
