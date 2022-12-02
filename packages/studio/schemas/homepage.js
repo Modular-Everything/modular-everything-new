cc5f632ec7c72d895bd2c779faa62245dd5b47b8
Chrish Dunne <yo@modular-everything.com>
2022-12-02 17:05:53 +0000

    feat: Added homepage schema

---
+++ packages/studio/schemas/homepage.js
@@ -0,0 +1,37 @@
+export default {
+  name: "homepage",
+  type: "document",
+  title: "Homepage",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "selected_works",
+      title: "Selected Works",
+      type: "string",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+    // {
+    //   name: "projects",
+    //   title: "Projects",
+    //   type: "array",
+    //   of: [
+    //     {
+    //       name: 'project',
+    //       title: 'Project',
+    //       type: 'reference',
+    //       to: [{
+    //         type: 'project'
+    //       }]
+    //     },
+    //   ],
+    // },
+  ],
+};
