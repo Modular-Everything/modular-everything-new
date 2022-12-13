6b826339e175c0a4d96b5b538c13216deb5e5e8b
Chrish Dunne <yo@modular-everything.com>
2022-12-13 09:25:13 +0000

    feat: Dynamically load in project data

--- packages/web/src/app/pages/Home.js
+++ packages/web/src/app/pages/Home.js
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import { getOffset } from "../utils/dom";
 import { Page } from "../classes/Page";
 import each from "lodash/each";
+import Sanity from "../data/sanity/Sanity";
 
 export const HomePage = class HomePage extends Page {
   constructor() {
@@ -13,12 +14,18 @@ export const HomePage = class HomePage extends Page {
         meta: ".home__meta",
         projects: ".home__projects__wrapper",
         content: ".home__content",
+        content_transition: ".home__content__transition",
         projectItems: ".home__projects__item",
+        project_meta_info: ".home__project__meta__info",
+        project_meta_caption_copy: ".home__project__meta__caption__copy",
+        project_meta_caption_link: ".home__project__meta__caption__link",
+        project_meta_media: ".home__project__meta__media",
       },
     });
 
     this.transformPrefix = Prefix("transform");
     this.onMouseWheelEvent = this.onMouseWheel.bind(this);
+    this.sanity = new Sanity();
   }
 
   /**
@@ -64,14 +71,40 @@ export const HomePage = class HomePage extends Page {
   }
 
   /**
+   * Update project content from Sanity data
+   */
+  async updateProjectContent(projectId) {
+    const data = await this.sanity.getProject(projectId).then((data) => {
+      this.timeline = gsap.timeline();
+
+      this.timeline.to(this.elements.content_transition, {
+        transformOrigin: "left",
+        scaleX: 1,
+        duration: 0.3,
+      });
+
+      this.timeline.call(() => {
+        this.elements.project_meta_caption_copy.innerHTML = data.title;
+      });
+
+      this.timeline.to(this.elements.content_transition, {
+        transformOrigin: "right",
+        scaleX: 0,
+        duration: 0.3,
+      });
+    });
+  }
+
+  /**
    * Create a navigation item from each project item
    */
 
   createNavFromElement(element) {
     const { projectId } = element.children[0].dataset;
-    element.children[0].addEventListener("click", () => {
+    element.children[0].addEventListener("click", async () => {
       document.body.dataset.selectedProject = projectId;
       this.setActiveNavElement(element, projectId);
+      this.updateProjectContent(projectId);
     });
   }
 
@@ -87,58 +120,19 @@ export const HomePage = class HomePage extends Page {
       button.classList.add("home__projects__item__link--active");
       const { projectId } = element.children[0].dataset;
       document.body.dataset.selectedProject = projectId;
+      this.updateProjectContent(projectId);
     }
   }
 
   /**
-   * Animate in and then set the active nav element
+   * Set the active element
    */
   setActiveNavElement(element) {
     const activeElement = this.elements.projects.querySelector(
       ".home__projects__item__link--active"
     );
-    const activeElementUnderline = activeElement.querySelector("span");
-
-    this.tl = gsap.timeline();
-
-    this.tl.to(activeElement, {
-      fontWeight: 400,
-      duration: 0.18,
-    });
-
-    this.tl.to(
-      activeElementUnderline,
-      {
-        scaleY: 0,
-        duration: 0.18,
-        ease: "expo.out",
-      },
-      "-=1"
-    );
-
-    this.tl.to(
-      element.querySelector("span"),
-      {
-        scaleY: 1,
-        duration: 0.18,
-        ease: "expo.in",
-      },
-      "-=2"
-    );
-
-    this.tl.to(
-      element.children[0],
-      {
-        fontWeight: 700,
-        duration: 0.18,
-      },
-      "-=2"
-    );
-
-    this.tl.call(() => {
-      activeElement.classList.remove("home__projects__item__link--active");
-      element.children[0].classList.add("home__projects__item__link--active");
-    });
+    activeElement.classList.remove("home__projects__item__link--active");
+    element.children[0].classList.add("home__projects__item__link--active");
   }
 
   /**
