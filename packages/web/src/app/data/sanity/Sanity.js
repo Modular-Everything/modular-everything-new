7e6873985fbd0659ab397adc7527569fcf7cf61e
Chrish Dunne <yo@modular-everything.com>
2022-12-13 09:24:50 +0000

    feat: Fetch project data

--- packages/web/src/app/data/sanity/Sanity.js
+++ packages/web/src/app/data/sanity/Sanity.js
@@ -1,23 +1,32 @@
 import SanityClient from "@sanity/client";
 
 export default class SanityHook {
-  async getData() {
-    const client = SanityClient({
-      projectId: process.env.VITE_SANITY_PROJECT_ID,
-      dataset: process.env.VITE_SANITY_DATASET,
+  constructor() {
+    this.client = SanityClient({
+      projectId: "5u4gnb55",
+      dataset: "production",
       apiVersion: "2022-12-02",
       useCdn: false,
     });
+  }
 
-    const homepage = await client.fetch(`
+  async getPages() {
+    const homepage = await this.client.fetch(`
       *[_type == 'homepage'][0] {
         ...,
         projects[]->
       }
     `);
 
-    console.log(homepage);
-
     return { homepage };
+  }
+
+  async getProject(id) {
+    return await this.client.fetch(
+      `
+        *[_type == 'project' && slug.current == $id][0]
+      `,
+      { id }
+    );
   }
 }
